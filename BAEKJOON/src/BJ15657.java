7ad594b0509649be250800e64058a5081c0fc0db
ZeroIRC <save3172@naver.com>
2022-12-29 02:02:32 +0900

    15657번 - N과 M (8)

---
+++ BAEKJOON/src/BJ15657.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+import java.util.function.Function;
+
+public class BJ15657 {
+
+    private static StringBuilder sb = new StringBuilder();
+    private static int[] inputs;
+    private static int[] outputs;
+    private static Function<String, Integer> toInt = Integer::parseInt;
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = toInt.apply(st.nextToken());
+        int m = toInt.apply(st.nextToken());
+        outputs = new int[m];
+        inputs = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; ++i) {
+            inputs[i] = toInt.apply(st.nextToken());
+        }
+        Arrays.sort(inputs);
+        dfs(n, m, 0, 0);
+        System.out.println(sb);
+
+    }
+
+    private static void dfs(int n, int m, int index, int count) {
+        if (count == m) {
+            for (int output : outputs) {
+                sb.append(output).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = index; i < n; ++i) {
+            outputs[count] = inputs[i];
+            dfs(n, m, i, count + 1);
+        }
+    }
+
+}
