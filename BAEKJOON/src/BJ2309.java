c5a1f78a83053618c8cbec73e64c62ef50cafac8
ZeroIRC <save3172@naver.com>
2023-01-05 19:59:38 +0900

    2309번 - 일곱 난쟁이 리팩토링

--- BAEKJOON/src/BJ2309.java
+++ BAEKJOON/src/BJ2309.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 import java.util.function.Function;
 
+
 public class BJ2309 {
     private static StringBuilder stringBuilder = new StringBuilder();
     private static int[] inputs;
@@ -28,15 +29,17 @@ public class BJ2309 {
         }
         Arrays.sort(inputs);
         dfs(DWARF_COUNT, REAL_DWARF, 0, 0);
-        System.out.println(stringBuilder);
-
-
+        System.out.print(stringBuilder);
     }
 
     private static void dfs(int n, int m, int index, int count) {
         if (count == m) {
             if (Arrays.stream(outputs).sum() == GOAL) {
                 for (int output : outputs) {
+                    if (output == outputs[outputs.length - 1]) {
+                        stringBuilder.append(output);
+                        continue;
+                    }
                     stringBuilder.append(output).append("\n");
                 }
             }
